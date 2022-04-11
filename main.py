1b3c126784ab10f0996b1b3f015b5d8953de60a7
Bad_karma <49790977+eeenvik1@users.noreply.github.com>
2022-04-11 09:42:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ YOU_TRACK_TOKEN = config.get("YOU_TRACK_TOKEN")
 YOU_TRACK_PROJECT_ID = config.get("YOU_TRACK_PROJECT_ID")
 YOU_TRACK_BASE_URL = config.get("YOU_TRACK_BASE_URL")
 URL = config.get("YOUR_URL2")
-#parse cvetrends.com------------------------------------------------------------------------------------------
+
+# parse cvetrends.com------------------------------------------------------------------------------------------
 def get_top_cve_list():
     link = 'https://cvetrends.com/api/cves/24hrs'
     r = requests.get(link)
@@ -28,7 +29,59 @@ def get_top_cve_list():
 
     return top_cve_list
 
-#add new tag for new issue------------------------------------------------------------------------------------
+
+# parse microsoft------------------------------------------------------------------------------------------
+def check_microsoft(cve):
+    msrc_url = f"https://api.msrc.microsoft.com/cvrf/v2.0/Updates('{cve}')"
+    get_cvrf_link = requests.get(msrc_url, verify=False)
+    return get_cvrf_link.status_code
+
+
+def get_kb(cve):
+    msrc_url = f"https://api.msrc.microsoft.com/cvrf/v2.0/Updates('{cve}')"
+    get_cvrf_link = requests.get(msrc_url, verify=False)
+    id_for_cvrf = re.search(r'\d{4}-\w{3}', get_cvrf_link.text)
+    cvrf_url = f'https://api.msrc.microsoft.com/cvrf/v2.0/document/{id_for_cvrf[0]}'
+    get_info = requests.get(cvrf_url, verify=False)
+    soup = BeautifulSoup(get_info.text, "html.parser")
+    parse_list = []
+    buff = ''
+    for item in soup.text:
+        if item == '\n':
+            parse_list.append(buff)
+            buff = ''
+        else:
+            buff += item
+    parse_string = ''
+    for j, item in enumerate(parse_list):
+        regex = re.findall(cve, parse_list[j])
+        if regex:
+            parse_string = parse_list[j]
+    kb_list = re.findall(r'KB\d{7}', parse_string)
+    not_remove_list_of_kb = []
+    for kb in kb_list:
+        if kb not in not_remove_list_of_kb:
+            not_remove_list_of_kb.append(kb)
+    link_list = []
+    for kb in not_remove_list_of_kb:
+        kb_url = f'https://catalog.update.microsoft.com/v7/site/Search.aspx?q={kb}'
+        test = requests.get(kb_url, verify=False)
+        if test.status_code == 200:
+            url_get_product = f'https://www.catalog.update.microsoft.com/Search.aspx?q={kb}'
+            get_product = requests.get(url_get_product, verify=False)
+            soup_get_product = BeautifulSoup(get_product.text, "html.parser")
+            product_buff = ''
+            for item in soup_get_product.find_all('a', class_='contentTextItemSpacerNoBreakLink'):
+                product_buff = item.text
+            product = product_buff.strip()
+            # link_list.append(f'[{kb}]({kb_url}) - {(product.partition("for")[2])[:-12]}') # Output: Windows 10 Version 1809 for x86-based Systems
+            if product:
+                link_list.append(f'[{kb}]({kb_url}) - {product}') # Output: 2022-01 Cumulative Update for Windows 10 Version 1809 for x86-based Systems (KB5009557)
+
+    return link_list
+
+
+# add new tag for new issue------------------------------------------------------------------------------------
 def add_tag(id):
     request_payload = {
         "project": {
@@ -46,13 +99,14 @@ def add_tag(id):
     diff = requests.post(url_differences, headers=headers, json=request_payload)
     return diff.status_code
 	
-#delete tag for issue-----------------------------------------------------------------------------------------
+# delete tag for issue-----------------------------------------------------------------------------------------
 def delete_tag(id):
     URL1 = f'{YOU_TRACK_BASE_URL}/issues/{id}/tags/6-27'
     delete = requests.delete(URL1, headers=headers)
     return delete.status_code
 
-#change tag for issue-----------------------------------------------------------------------------------------
+
+# change tag for issue-----------------------------------------------------------------------------------------
 def change_tag(id):
     request_payload = {
         "project": {
@@ -70,7 +124,8 @@ def change_tag(id):
     diff = requests.post(url_differences, headers=headers, json=request_payload)
     return diff.status_code
 
-#parse github/nu11secur1ty------------------------------------------------------------------------------------
+
+# parse github/nu11secur1ty------------------------------------------------------------------------------------
 def get_exploit_info(cve):
     link = 'https://github.com/nu11secur1ty/CVE-mitre'
     link_2 = 'https://github.com/nu11secur1ty/CVE-mitre/tree/main/2022'
@@ -95,7 +150,8 @@ def get_exploit_info(cve):
             default_link = f'https://github.com/nu11secur1ty/CVE-mitre/tree/main/{cve}'
     return default_link
 
-#main function for parse cve----------------------------------------------------------------------------------
+
+# main function for parse cve----------------------------------------------------------------------------------
 def get_cve_data(cve):
     template = """
 ### Описание
@@ -110,12 +166,14 @@ def get_cve_data(cve):
 
 {{d.publishedDate}}
 
+
 ### Продукт, вендор
 
 <details>
 
 {% for vendor in d.product_vendor_list %}{{vendor}}
 {% endfor %}
+
 
 </details>
 
@@ -157,6 +215,18 @@ def get_cve_data(cve):
 
 </details>
 
+
+{%if d.kb_links %}
+
+### Решение от майкрософт
+<details>
+<summary>Установить следующие обновления безопасности</summary>
+
+{% for link in d.kb_links %}{{link}}
+{% endfor %}
+{% endif %}
+
+</details>
     """
 
     YOU_TRACK_PROJECT_ID = config.get("YOU_TRACK_PROJECT_ID")
@@ -176,7 +246,14 @@ def get_cve_data(cve):
         r = nvdlib.getCVE(cve, cpe_dict=False)
         cve_cpe_nodes = r.configurations.nodes
         cpe_nodes = ast.literal_eval(str(r.configurations))
-        score = r.v3score
+        try:
+            score = r.v3score
+            vector = r.v3vector
+        except:
+            score = 0.1
+            vector = "Нет: cvss vector"
+        if vector != "Нет: cvss vector":
+            vector = r.v3vector[9:len(r.v3vector)]
         links = []
         exploit_links = []
         links.append(r.url)
@@ -197,7 +274,7 @@ def get_cve_data(cve):
                     for cpe in child.cpe_match:
                         cpe_for_product_vendors.append(cpe.cpe23Uri)
 
-    #parse CPE--------------------------------------------------------------------------------------------------------------
+    # parse CPE--------------------------------------------------------------------------------------------------------------
         product_vendor_list = []
         product_image_list = []
         version_list = []
@@ -235,7 +312,7 @@ def get_cve_data(cve):
         if not exploit_links:
             value = "Нет"
 
-    #check regex in cve-----------------------------------------------------------------------------------------------------
+    # check regex in cve-----------------------------------------------------------------------------------------------------
         cve_name = ''
         cve_info = r.cve.description.description_data[0].value
         for item in pattern:
@@ -244,7 +321,12 @@ def get_cve_data(cve):
                 break
             else:
                 cve_name = cve
-    #message----------------------------------------------------------------------------------------------------------------
+                
+    # check kb in cve------------------------------------------------------------------------------------------------
+        kb_links = ''
+        if check_microsoft(cve) == 200:
+            kb_links = get_kb(cve)
+    # message----------------------------------------------------------------------------------------------------------------
         data = {
             'cve': cve_info,
             'lastModifiedDate': r.lastModifiedDate[:-7],
@@ -254,11 +336,12 @@ def get_cve_data(cve):
             'vector': r.v3vector,
             'links': links,
             'product_vendor_list': prod,
-            'exploit_links': exploit_links
+            'exploit_links': exploit_links,
+            'kb_links': kb_links
         }
         message = jinja2.Template(template).render(d=data)
 
-    #check for product_vendor-----------------------------------------------------------------------------------------------
+    # check for product_vendor-----------------------------------------------------------------------------------------------
         URL_get_products = config.get("URL_GET_PRODUCTS")
         headers = {
             "Accept": "application/json",
@@ -317,35 +400,36 @@ def get_cve_data(cve):
     # upload information on cve---------------------------------------------------------------------------------------------
         buff_content = []
         buff_versions = []
-        if re.search(r'windows', product_vendor_list[0]):
-            con = {"name": "Microsoft Windows"}
-            buff_content.append(con)
-            buff_versions = versions
-        elif re.search(r'juniper', product_vendor_list[0]):
-            con = {"name": "Juniper"}
-            buff_content.append(con)
-            buff_versions = versions
-        elif re.search(r'adaptive_security_appliance', product_vendor_list[0]):
-            con = {"name": "Cisco ASA"}
-            buff_content.append(con)
-            buff_versions = versions
-        else:
-            if content:
-                buff_content.append(content[0])
-            if versions:
-                buff_versions.append(versions[0])
+        if product_vendor_list:
+            if re.search(r'windows', str(product_vendor_list[0])):
+                con = {"name": "Microsoft Windows"}
+                buff_content.append(con)
+                buff_versions = versions
+            elif re.search(r'juniper', str(product_vendor_list[0])):
+                con = {"name": "Juniper"}
+                buff_content.append(con)
+                buff_versions = versions
+            elif re.search(r'adaptive_security_appliance', str(product_vendor_list[0])):
+                con = {"name": "Cisco ASA"}
+                buff_content.append(con)
+                buff_versions = versions
+            else:
+                if content:
+                    buff_content.append(content[0])
+                if versions:
+                    buff_versions.append(versions[0])
 
         priority = ''
-        if score >= 0.1 and score <= 3.9:
-            priority = 'Низкая'
-        elif score >= 4.0 and score <= 6.9:
-            priority = 'Средняя'
-        elif score >= 7.0 and score <= 8.9:
-            priority = 'Высокая'
-        elif score >= 9.0 and score <= 10.0:
-            priority = 'Критическая'
+        if isinstance(score, float):
+            if 0.1 <= score <= 3.9:
+                priority = 'Низкая'
+            elif 4.0 <= score <= 6.9:
+                priority = 'Средняя'
+            elif 7.0 <= score <= 8.9:
+                priority = 'Высокая'
+            elif 9.0 <= score <= 10.0:
+                priority = 'Критическая'
 
-        vector = r.v3vector[9:len(r.v3vector)]
         request_payload = {
             "project": {
                 "id": YOU_TRACK_PROJECT_ID
@@ -399,7 +483,7 @@ def get_cve_data(cve):
     except:
         pass
 
-#email alerting----------------------------------------------------------------------------------------------
+# email alerting----------------------------------------------------------------------------------------------
 def email_alert(cve_list):
     EMAIL_HOST = config.get("EMAIL_HOST")
     EMAIL_PORT = config.get("EMAIL_PORT")
@@ -409,7 +493,7 @@ def email_alert(cve_list):
     msg['Subject'] = 'cvetrend.com'
     msg['From'] = EMAIL_HOST_USER
     msg['To'] = ", ".join(recipients)
-    if len(cve_list) == 1:
+    if cve_list:
         body = f'Добавлена информация о новой уязвимости {cve_list[0]}'
     else:
         body = f'Добавлена информация о новых уязвимостях\n {cve_list}'
@@ -429,12 +513,12 @@ headers = {
     "Content-Type": "application/json"
 }
 
-list_summary = requests.get(URL, headers=headers).json() # Получение задач с YouTrack
+list_summary = requests.get(URL, headers=headers).json()  # Получение задач с YouTrack
 
-#Получение информации по cve с YouTrack
-cve_list = [] #CVE id
-id_list = []  #ID Задачи
-tag_id = []   #ID Тэга
+# Получение информации по cve с YouTrack
+cve_list = [] # CVE id
+id_list = []  # ID Задачи
+tag_id = []   # ID Тэга
 for i in range(len(list_summary)):
     regex = re.search(r'CVE-\d{4}-\d{4,8}', str(list_summary[i]['summary']))
     if regex != None:
@@ -446,9 +530,9 @@ for i in range(len(list_summary)):
         except:
             tag_id.append('NO')
 
-top_cve_list = get_top_cve_list() #Получение топа cve с cvetrends.com
+top_cve_list = get_top_cve_list() # Получение топа cve с cvetrends.com
 
-#Добавление тега к задачам
+# Добавление тега к задачам
 print(f'Добавление тега:')
 repetiotion_cve = []
 for i in range(len(cve_list)):
@@ -457,13 +541,13 @@ for i in range(len(cve_list)):
             add_tag(id_list[i])
             repetiotion_cve.append(top_cve_list[j])
 
-#Удаление тега с задач, для которых он не актуален и добавление другого тега
+# Удаление тега с задач, для которых он не актуален и добавление другого тега
 remove_list = []
 delete_list = []
 del_list = []
 print(f'Удаление и изменение тега:')
 for i in range(len(cve_list)):
-    if tag_id[i] == '6-27': #id искомого тэга (В тренде)
+    if tag_id[i] == '6-27':  # id искомого тэга (В тренде)
         remove_list.append(cve_list[i])
 for item in remove_list:
     if item not in top_cve_list:
@@ -478,14 +562,14 @@ if del_list:
 else:
     print('None')
 
-#Формирование списка уязвимостей (CVE), которых нет в YouTrack
+# Формирование списка уязвимостей (CVE), которых нет в YouTrack
 add_new_cve = []
 for item in top_cve_list:
     if item not in repetiotion_cve:
         add_new_cve.append(item)
 
 email_list = []
-#Добавление информации о новых уязвимостях в YouTrack
+# Добавление информации о новых уязвимостях в YouTrack
 print(f'Добавление информации о новых уязвиомстях:')
 if add_new_cve:
     for i in range(len(add_new_cve)):
@@ -495,6 +579,6 @@ if add_new_cve:
 else:
     print('None')
 
-#Увемодление на почту
+# Увемодление на почту
 if email_list:
     email_alert(email_list)
